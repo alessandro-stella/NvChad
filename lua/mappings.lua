ec4bcb2bc818e0590650880bb638fb4d9bfd0740
alessandro-stella <alessandro.stella2004@gmail.com>
2025-07-03 16:51:09 +0200

    Commented keymap for j to avoid lag during insert mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 
+-- map("i", "k", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
