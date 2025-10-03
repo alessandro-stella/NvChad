55cded7e186a259c350f86dd770c54732e07e2a9
alessandro-stella <alessandro.stella2004@gmail.com>
2025-10-03 22:19:26 +0200

    Pre adding jdtls_config

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,21 @@
 require "nvchad.autocmds"
+
+-- Show dashboard on last buffer close
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.schedule(function()
+        vim.cmd "Nvdash"
+      end)
+    end
+  end,
+})
+
+-- Configure jdtls for java projects
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  callback = function(_)
+    require("configs.jdtls_setup").setup()
+  end,
+})
