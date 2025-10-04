f4377d1c16cc7c893caecb8c3c78651cc74ca2fa
alessandro-stella <alessandro.stella2004@gmail.com>
2025-10-04 17:54:26 +0200

    Nothing

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,33 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+local opt = vim.opt
+
+-- Disable visual line wrapping to prevent lines from breaking prematurely
+o.wrap = false
+
+-- Show cursor line highlighting for both screen line and line number
+o.cursorlineopt = "both"
+
+-- Enable absolute line numbers
 o.number = true
+
+-- Enable relative line numbers
 o.relativenumber = true
+
+-- Disable automatic line breaks when typing beyond a certain width
+opt.textwidth = 0
+
+-- Disable linebreak option which controls where lines break visually (only relevant if wrap is enabled)
+opt.linebreak = false
+
+-- Enable horizontal scrolling instead of wrapping when lines exceed screen width
+opt.sidescroll = 1
+
+-- Keep a margin of 5 columns when scrolling horizontally
+opt.sidescrolloff = 5
+
+-- Show at least 5 rows before and after the cursor
+opt.scrolloff = 5
+
+opt.winborder = "single"
