a1ea235f653131001a7f9e9b0fff00462d5a8ef2
alessandro-stella <alessandro.stella2004@gmail.com>
2025-11-15 10:51:35 +0100

    Added formatting to solidity

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,12 +1,12 @@
 local options = {
   forge = {
     command = "forge",
-    args = { "fmt", "--stdin" }, -- Usa stdin per lavorare con conform.nvim
+    args = { "fmt", "--stdin" },
   },
 
   formatters_by_ft = {
     lua = { "stylua" },
-    solidity = { "forge" },
+    solidity = { "forge_fmt" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
