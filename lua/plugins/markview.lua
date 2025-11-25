ae4cbf484a1088b375c8f4feee77a91eea232c64
alessandro-stella <alessandro.stella2004@gmail.com>
2025-11-25 10:26:32 +0100

    Added LaTeX preview to markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,5 +1,23 @@
 return {
   "OXY2DEV/markview.nvim",
   lazy = false,
-  dependencies = { "saghen/blink.cmp" },
+  dependencies = {
+    "saghen/blink.cmp",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("markview").setup {
+      latex = {
+        enable = true, -- Abilita il rendering generale di LaTeX
+        inline = {
+          enable = true, -- Abilita la matematica inline ($...$)
+        },
+        block = {
+          enable = true, -- Abilita i blocchi matematici ($$...$$)
+          -- Puoi personalizzare l'aspetto del blocco se vuoi (opzionale)
+          -- text = { "  LaTeX ", "Special" },
+        },
+      },
+    }
+  end,
 }
